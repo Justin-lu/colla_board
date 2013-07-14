bba24a1e138558c087685ba43d3d73bec1d71f1a
yuanxin <yuanxin.qiu@easyread.ly>
2013-07-15 01:06:21 +0800

    Use Minitest instead of UnitTest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,23 @@
-ENV["RAILS_ENV"] ||= "test"
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+ENV["RAILS_ENV"] = "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
+
+require 'minitest/autorun'
+require "minitest/rails"
+require 'minitest/spec'
+require 'ffaker'
+
+DatabaseCleaner.strategy = :transaction
 
 class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
+  extend MiniTest::Spec::DSL # Add some RSpec style helper
+  include FactoryGirl::Syntax::Default
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
+  setup do
+    DatabaseCleaner.start
+  end
 
-  # Add more helper methods to be used by all tests here...
+  teardown do
+    DatabaseCleaner.clean
+  end
 end
