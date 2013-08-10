d734b82e8b1b0592787c4cd1b58db4bf39b9a4fb
yuanxin <yuanxin.qiu@easyread.ly>
2013-08-11 02:25:43 +0800

    User list view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,12 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
+    @users = User.all
   end
 
   # GET /users/:id
   def show
+    @user = User.find params[:id]
   end
 
 end
