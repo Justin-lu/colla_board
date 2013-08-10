e12d5f6926293ba5da64359853a83d408dd8f357
yuanxin <yuanxin.qiu@easyread.ly>
2013-08-10 20:50:04 +0800

    Remove jcrop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,2 @@
 class User < ActiveRecord::Base
-
-  validates_presence_of :name, :avatar
-
-  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
-  after_update :reprocess_avatar, if: :cropping?
-  mount_uploader :avatar, AvatarUploader
-
-  def cropping?
-    !crop_x.blank? && !crop_y.blank? && !crop_w.blank? && !crop_h.blank?
-  end
-
-  private
-  def reprocess_avatar
-    self.avatar.recreate_versions!
-  end
 end
